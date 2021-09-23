c6f76ff21f24ea382749229d3d28cc57be2250d0
Sree Harsha <harsha@virtualdusk.com>
2021-09-23 13:52:32 +0530

    Initial commit

---
+++ TripManagement/HomeVC.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  TripManagement
+//
+//  Created by Virtual Dusk  on 23/09/21.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    @IBOutlet weak var startTripBtn: UIButton!
+    
+    @IBOutlet weak var stopTripBtn: UIButton!
+    
+    @IBOutlet weak var currentTripName: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func onClickStartBtn(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func onClickStopBtn(_ sender: UIButton) {
+    }
+    
+    @IBAction func onTapViewAllTrips(_ sender: UIButton) {
+    }
+    
+}
+
